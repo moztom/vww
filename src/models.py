73d8fd626adb8145ba792c87bc7fcc4a1a30b33d
Tom Morris <tommorris455@gmail.com>
2025-10-27 21:59:14 +0000

    add efficientnet teacher

--- src/models.py
+++ src/models.py
@@ -2,7 +2,10 @@ import torch.nn as nn
 from torchvision.models import (
     mobilenet_v3_small,
     mobilenet_v3_large,
+    efficientnet_b2,
     MobileNet_V3_Large_Weights,
+    MobileNet_V3_Small_Weights,
+    EfficientNet_B2_Weights,
 )
 
 
@@ -10,13 +13,20 @@ def build_model(model_type, pretrained=False):
     """Build a model based on the specified architecture"""
 
     if model_type == "mobilenet_v3_small":
-        model = mobilenet_v3_small(weights=None)
+        weights = MobileNet_V3_Small_Weights.DEFAULT if pretrained else None
+        model = mobilenet_v3_small(weights=weights)
         model.classifier[-1] = nn.Linear(model.classifier[-1].in_features, 2)
         return model
 
     elif model_type == "mobilenet_v3_large":
         weights = MobileNet_V3_Large_Weights.DEFAULT if pretrained else None
         model = mobilenet_v3_large(weights=weights)
+        model.classifier[-1] = nn.Linear(model.classifier[-1].in_features, 2)
+        return model
+
+    elif model_type == "efficientnet_b2":
+        weights = EfficientNet_B2_Weights.DEFAULT if pretrained else None
+        model = efficientnet_b2(weights=weights)
         model.classifier[-1] = nn.Linear(model.classifier[-1].in_features, 2)
         return model
 
